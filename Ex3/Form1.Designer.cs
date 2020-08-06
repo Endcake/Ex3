e585a30cbf63574834338c8d50e7ef2f4d3bad0f
Christopher Kren <christopher.kren@gmail.com>
2020-08-06 15:10:19 +0200

    test

--- Ex3/Form1.Designer.cs
+++ Ex3/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 0;
-            this.btnExit.Text = "exit";
+            this.btnExit.Text = "exit app";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
